034921ff9de89484a3edb82e1a8acbff2c36b0ca
janinene <janinem.usana@gmail.com>
2022-10-18 19:09:29 -0400

    adding our dependency lock file

---
+++ build/precache-manifest.b6b0725d5176020568437de45c4a3a1b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5c10338d8f44c8f38230",
+    "url": "/janinene/COMP3104/static/css/main.aa0e659e.chunk.css"
+  },
+  {
+    "revision": "5c10338d8f44c8f38230",
+    "url": "/janinene/COMP3104/static/js/main.5c10338d.chunk.js"
+  },
+  {
+    "revision": "8d699b64bd52fbb89c6b",
+    "url": "/janinene/COMP3104/static/js/runtime~main.8d699b64.js"
+  },
+  {
+    "revision": "8ebec04f6c698faf83f8",
+    "url": "/janinene/COMP3104/static/js/2.8ebec04f.chunk.js"
+  },
+  {
+    "revision": "769095d2551dab9ef9469a75546fc6d1",
+    "url": "/janinene/COMP3104/index.html"
+  }
+];
