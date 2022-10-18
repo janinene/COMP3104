4e56a35fe80e913247be569a5900a9fe066a8e2c
janinene <janinem.usana@gmail.com>
2022-10-18 18:50:27 -0400

    commit all

---
+++ build/precache-manifest.31bad716f09011b0c4019fec9e17f9e6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9f8d8ca6cb7fa77fdd78",
+    "url": "/janinene/COMP3104/static/css/main.a330a44f.chunk.css"
+  },
+  {
+    "revision": "9f8d8ca6cb7fa77fdd78",
+    "url": "/janinene/COMP3104/static/js/main.9f8d8ca6.chunk.js"
+  },
+  {
+    "revision": "8d699b64bd52fbb89c6b",
+    "url": "/janinene/COMP3104/static/js/runtime~main.8d699b64.js"
+  },
+  {
+    "revision": "8ebec04f6c698faf83f8",
+    "url": "/janinene/COMP3104/static/js/2.8ebec04f.chunk.js"
+  },
+  {
+    "revision": "b2778359d2c94ad96b772685b5a36bfb",
+    "url": "/janinene/COMP3104/index.html"
+  }
+];
